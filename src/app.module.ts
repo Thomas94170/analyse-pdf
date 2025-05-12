8da02a27119f42b5c1e10c4e7301a4358496edab
Thomas Thirion <thirionthomas68100@gmail.com>
2025-05-12 20:06:58 +0200

    debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { OcrService } from './ocr/ocr.service';
 import { PdfService } from './pdftoimg/pdftoimg.service';
 import { IncomeService } from './income/income.service';
 import { IncomeModule } from './income/income.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { InvoiceService } from './invoice/invoice.service';
 
 @Module({
-  imports: [DocumentModule, IncomeModule],
+  imports: [DocumentModule, IncomeModule, InvoiceModule],
   controllers: [],
-  providers: [OcrService, PdfService, IncomeService],
+  providers: [OcrService, PdfService, IncomeService, InvoiceService],
 })
 export class AppModule {}
--- src/invoice/invoice.service.ts
+++ src/invoice/invoice.service.ts
@@ -6,6 +6,16 @@ export class InvoiceService {
   constructor(private readonly prisma: PrismaService) {}
 
   async readAllInvoices() {
-    
+    const allInvoices = await this.prisma.invoice.findMany({
+      select: {
+        id: true,
+        client: true,
+        totalBT: true,
+        totalInclTax: true,
+        dueDate: true,
+        phoneNumber: true,
+      },
+    });
+    return allInvoices;
   }
 }

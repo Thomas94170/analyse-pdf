9099e4c4c32520aa6da84fe1b6dbf0a6301c24d5
Thomas Thirion <thirionthomas68100@gmail.com>
2025-05-14 13:47:52 +0200

    add: date parsed

--- src/income/income.controller.ts
+++ src/income/income.controller.ts
@@ -10,7 +10,11 @@ export class IncomeController {
   ) {}
 
   @Get('annual-income')
-  async annualIncome(@Query('year') year: number) {
+  async annualIncome(@Query('year') yearString: string) {
+    const year = parseInt(yearString, 10);
+    if (isNaN(year)) {
+      throw new BadRequestException('Year must be a valid number');
+    }
     try {
       const result = await this.incomeService.annualIncome(year);
       return result;
@@ -20,7 +24,11 @@ export class IncomeController {
   }
 
   @Get('annual-taxation')
-  async annualTaxation(@Query('year') year: number) {
+  async annualTaxation(@Query('year') yearString: string) {
+    const year = parseInt(yearString, 10);
+    if (isNaN(year)) {
+      throw new BadRequestException('Year must be a valid number');
+    }
     try {
       const result = await this.incomeService.annualTaxation(year);
       if (!result) {

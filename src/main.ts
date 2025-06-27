d49e3a25026b3455f7d0ff2d2df4b0309cdec390
Thomas Thirion <thirionthomas68100@gmail.com>
2025-06-27 17:54:59 +0200

    add : verify

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,6 @@ async function bootstrap() {
   console.log('running on PORT, ', process.env.PORT ?? 3000);
   console.log('running on db, ', process.env.DATABASE_URL);
   console.log(process.env.JWT_SECRET);
+  console.log(process.env.DATABASE_URL);
 }
 bootstrap();

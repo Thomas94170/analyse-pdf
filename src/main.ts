57cb9a335e7cb3bd8f94687102803fa5a94bfa3e
Thomas Thirion <thirionthomas68100@gmail.com>
2025-06-28 19:14:01 +0200

    add : path

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://si-front-x8bk.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://si-front-x8bk.vercel.app',
+      'https://www.si-front-x8bk.vercel.app',
+    ],
     credentials: true,
   });
 
